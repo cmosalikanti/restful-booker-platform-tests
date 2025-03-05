025226609958577cb7e786ce3d62aa02423fff2b
Chandra Sekhar Mosalikanti <mcsekhar777@gmail.com>
2025-03-05 11:58:08 +0000

    refactoring to extract the duplicate locators as variables

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -13,19 +13,33 @@ test('should verify the contents on welcome page section', async ({ page }) => {
   await expect(page.locator('#collapseBanner')).toContainText('Infrastructure:');
   await expect(page.locator('#collapseBanner')).toContainText('Get Started:');
 
-  await expect(page.getByRole('link', { name: 'restful-booker-platform source code' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'restful-booker-platform source code' })).toHaveAttribute("href", "https://github.com/mwinteringham/restful-booker-platform");
-  await expect(page.getByRole('link', { name: 'restful-booker-platform source', exact: true })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'build process in this public build pipeline' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'build process in this public build pipeline' })).toHaveAttribute("href", "https://circleci.com/gh/mwinteringham/workflows/restful-booker-platform");
-  await expect(page.getByRole('link', { name: 'home page' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'home page' })).toHaveAttribute("href", "https://automationintesting.online");
-  await expect(page.getByRole('link', { name: 'admin panel', exact: true })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'admin panel', exact: true })).toHaveAttribute("href", "https://automationintesting.online/#/admin");
-  await expect(page.getByRole('link', { name: 'read more about the features here', exact: true })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'read more about the features here', exact: true })).toHaveAttribute("href", "https://github.com/mwinteringham/restful-booker-platform/projects/1");
-  await expect(page.getByRole('link', { name: 'feel free to raise it here' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'feel free to raise it here' })).toHaveAttribute("href", "https://github.com/mwinteringham/restful-booker-platform/issues");
+  let locatorForPlatformSourceCode = page.getByRole('link', { name: 'restful-booker-platform source code', exact: true })
+  await expect(locatorForPlatformSourceCode).toBeVisible();
+  await expect(locatorForPlatformSourceCode).toHaveAttribute("href", "https://github.com/mwinteringham/restful-booker-platform");
+
+  let locatorForPlatformSource = page.getByRole('link', { name: 'restful-booker-platform source', exact: true })
+  await expect(locatorForPlatformSource).toBeVisible();
+  await expect(locatorForPlatformSource).toHaveAttribute("href", "https://github.com/mwinteringham/restful-booker-platform");
+
+  let locatorForBuildPipeline = page.getByRole('link', { name: 'build process in this public build pipeline' })
+  await expect(locatorForBuildPipeline).toBeVisible();
+  await expect(locatorForBuildPipeline).toHaveAttribute("href", "https://circleci.com/gh/mwinteringham/workflows/restful-booker-platform");
+
+  let locatorForHomePageLink = page.getByRole('link', {name: 'home page', exact: true});
+  await expect(locatorForHomePageLink).toBeVisible();
+  await expect(locatorForHomePageLink).toHaveAttribute("href", "https://automationintesting.online");
+
+  let locatorForAdminPanelLink = page.getByRole('link', {name: 'admin panel', exact: true});
+  await expect(locatorForAdminPanelLink).toBeVisible();
+  await expect(locatorForAdminPanelLink).toHaveAttribute("href", "https://automationintesting.online/#/admin");
+
+  let locatorForReadMoreFeaturesLink = page.getByRole('link', {name: 'read more about the features here', exact: true})
+  await expect(locatorForReadMoreFeaturesLink).toBeVisible();
+  await expect(locatorForReadMoreFeaturesLink).toHaveAttribute("href", "https://github.com/mwinteringham/restful-booker-platform/projects/1");
+
+  let locatorForFeelFreeToRaiseItHereLink = page.getByRole('link', {name: 'feel free to raise it here', exact: true})
+  await expect(locatorForFeelFreeToRaiseItHereLink).toBeVisible();
+  await expect(locatorForFeelFreeToRaiseItHereLink).toHaveAttribute("href", "https://github.com/mwinteringham/restful-booker-platform/issues");
 
   await expect(page.getByRole('button', { name: 'Let me hack!' })).toBeVisible();
 });
